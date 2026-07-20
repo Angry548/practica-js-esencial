62015273e4e2fe439c73c22a4a606c62e3c3bf7b
Angry548 <angelmate827@gmail.com>
2026-07-20 17:41:09 -0600

    Creacion del ejercicio 2 y modificacion del 1

---
+++ scripts/ejercicio2.js
@@ -0,0 +1,8 @@
+let resultado = 15 % 4;
+
+console.log("El residuo de 15 entre 4 es:", resultado);
+
+document.write("<h2>Resultado</h2>");
+document.write("<p>15 % 4 = " + resultado + "</p>");
+
+alert("El residuo es: " + resultado);
