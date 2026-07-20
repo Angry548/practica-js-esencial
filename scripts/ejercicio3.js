b631c033212a9992457147abe547f03ffe584d18
Angry548 <angelmate827@gmail.com>
2026-07-20 17:43:54 -0600

    Se agrega el ejercicio 3 sobre operadores de asignación

---
+++ scripts/ejercicio3.js
@@ -0,0 +1,11 @@
+let puntaje = 100;
+
+puntaje += 50;
+
+console.log("El nuevo puntaje es:", puntaje);
+
+document.write("<h2>Resultado</h2>");
+document.write("<p>Puntaje inicial: 100</p>");
+document.write("<p>Puntaje final: " + puntaje + "</p>");
+
+alert("El nuevo puntaje es: " + puntaje);
