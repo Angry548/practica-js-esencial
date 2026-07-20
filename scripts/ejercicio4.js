63e4a2977ec608bbc3f57d0100f280e73010bf8d
Angry548 <angelmate827@gmail.com>
2026-07-20 17:47:37 -0600

    Se agrega el ejercicio 4 de concatenación de cadenas

---
+++ scripts/ejercicio4.js
@@ -0,0 +1,13 @@
+let saludo = "Hola ";
+let usuario = "Carlos";
+
+let mensaje = saludo + usuario;
+
+console.log("Mensaje:", mensaje);
+
+document.write("<h2>Resultado</h2>");
+document.write("<p>Saludo: " + saludo + "</p>");
+document.write("<p>Usuario: " + usuario + "</p>");
+document.write("<p>Mensaje: " + mensaje + "</p>");
+
+alert(mensaje);
