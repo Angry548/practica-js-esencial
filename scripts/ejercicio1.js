039826b6f3e5752782dc4781b6d4f0a8b230d4f8
Angry548 <angelmate827@gmail.com>
2026-07-20 17:34:19 -0600

    Creacion del ejercicio1

---
+++ scripts/ejercicio1.js
@@ -0,0 +1,12 @@
+let base = 10;
+let altura = 5;
+
+let area = base * altura;
+
+console.log("El área del rectángulo es:", area);
+document.write("<h2>Resultado</h2>");
+document.write("<p>Base: " + base + "</p>");
+document.write("<p>Altura: " + altura + "</p>");
+document.write("<p>Área: " + area + "</p>");
+
+alert("El área del rectángulo es: " + area);
