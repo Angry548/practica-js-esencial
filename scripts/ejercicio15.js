779c5798a577c9efaa21f732fa35ff442e655874
Angry548 <angelmate827@gmail.com>
2026-07-20 19:06:21 -0600

    Se agrega el ejercicio 15 con bucle for...of

---
+++ scripts/ejercicio15.js
@@ -0,0 +1,8 @@
+const colores = ["Rojo", "Azul", "Verde"];
+
+for (const color of colores) {
+    console.log(color);
+    document.write(color + "<br>");
+}
+
+alert("El recorrido del arreglo ha finalizado.");
