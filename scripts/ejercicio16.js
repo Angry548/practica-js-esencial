4c6953fd39b50aca7a6526dc8eadc415d7a157c6
Angry548 <angelmate827@gmail.com>
2026-07-20 19:08:56 -0600

    Se agrega el ejercicio 16 con bucle do...while

---
+++ scripts/ejercicio16.js
@@ -0,0 +1,8 @@
+let numero = 1;
+
+do {
+    console.log("El bloque se ejecutó.");
+    document.write("El bloque se ejecutó.<br>");
+} while (numero > 5);
+
+alert("El ciclo do...while ha finalizado.");
