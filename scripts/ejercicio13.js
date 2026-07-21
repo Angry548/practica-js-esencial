5fa67216d45b9e3bc59d2404645a50d88119fb91
Angry548 <angelmate827@gmail.com>
2026-07-20 19:01:02 -0600

    Se agrega el ejercicio 13 con bucle while

---
+++ scripts/ejercicio13.js
@@ -0,0 +1,9 @@
+let numero = 2;
+
+while (numero <= 10) {
+    console.log(numero);
+    document.write(numero + "<br>");
+    numero += 2;
+}
+
+alert("El bucle ha finalizado. Revisa la consola para ver los números pares.");
