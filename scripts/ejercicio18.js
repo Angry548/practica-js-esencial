a1d2663a3c1c3224622a720047cafbe3ae9da135
Angry548 <angelmate827@gmail.com>
2026-07-20 19:14:39 -0600

    Se agrega el ejercicio 18 con función flecha

---
+++ scripts/ejercicio18.js
@@ -0,0 +1,10 @@
+const saludarUsuario = (nombre) => "Hola, " + nombre;
+
+let mensaje = saludarUsuario("Carlos");
+
+console.log(mensaje);
+
+document.write("<h2>Resultado</h2>");
+document.write("<p>" + mensaje + "</p>");
+
+alert(mensaje);
